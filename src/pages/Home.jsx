602651b5c517a9917fc5e644719b517948b4f7da
Triador 220 <killer34445@mail.ru>
2023-06-19 17:02:31 +0500

    release v1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
+import { setCategoryId, setCurrentPage } from "../redux/slices/filterslice";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { categoryId, sort, currentPage } = useSelector(
+    (state) => state.filter
+  );
+
+  const { searchValue } = React.useContext(SearchContext);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, seIsLoading] = React.useState(false);
+
+  const onChangeCategory = (id) => {
+    dispatch(setCategoryId(id));
+  };
+
+  const onChangePage = (number) => {
+    dispatch(setCurrentPage(number));
+  };
+
+  React.useEffect(() => {
+    seIsLoading(true);
+
+    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const sortBy = sort.sortProperty.replace("-", "");
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+
+    axios
+      .get(
+        `https://6446ec387bb84f5a3e351f7a.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setItems(res.data);
+        seIsLoading(false);
+      });
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import filter from './slices/filterslice';
+import cart from './slices/cartSlice';
+
+
+export const store = configureStore({
+  reducer: {
+    filter,
+    cart,
+  },
+});
